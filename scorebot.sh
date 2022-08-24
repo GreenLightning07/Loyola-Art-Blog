b4fb6e10b0d3cf74a9e1673995bdfbc200dc7c69
GreenLightning07 <53065122+GreenLightning07@users.noreply.github.com>
2022-08-24 13:09:22 -0400

    Update scorebot.sh

--- scorebot.sh
+++ scorebot.sh
@@ -76,7 +76,7 @@ function notify-send()
 function check()
 {
 	if ( eval $1 ); then
-		if ( cat $score_report | grep 'id="$2"' | grep "display:none" ); then
+		if ( cat $score_report | grep "id=\"$2\"" | grep "display:none" ); then
 			show-vuln '$2' "Vuln$2;" "$3" $4
 		fi
 	elif ( cat $score_report | grep "id=\"$2\"" | grep "display:block" ); then
