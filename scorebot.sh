9ca15481915d3886c13ca884f408eaf2f8f4cf66
GreenLightning07 <53065122+GreenLightning07@users.noreply.github.com>
2022-11-10 10:24:13 -0500

    Update scorebot.sh

--- scorebot.sh
+++ scorebot.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
 total_found=0
+total_percent=""
+total_pen=0
 
-score_report="/home/USERNAME/Desktop/ScoreReport.html"
-tail /usr/local/bin/xxzz.txt
-total_found=`cat /usr/local/bin/xxzz.txt`
+pam_configed=false
+encrypt_set=false
+
+score_report="/home/skipper/Desktop/ScoreReport.html"
+
 function update-found
 {
 	#updates vuln found counts in score report
+	total_percent=$(awk -vn=$total_found 'BEGIN{print(n*2.00)}')
+	echo $total_percent
+        sed -i "s/id=\"total_found\".*/id=\"total_found\">$total_found\/50<\/h3>/g" $score_report
+        sed -i "s/id=\"total_percent\".*/id=\"total_percent\">$total_percent%<\/h3>/g" $score_report
 	
-        sed -i "s/id=\"total_found\".*/id=\"total_found\">$total_found\/54<\/center><\/h3>/g" $score_report
-
-	echo $total_found > /usr/local/bin/xxzz.txt
+	if ( $total_pen == 0 ); then
+		sed -i "s/id=\"p0\"style=\"display:none\"/id=\"p0\"style=\"display:block\"/g" $score_report
+	else
+		sed -i "s/id=\"p0\"style=\"display:block\"/id=\"p0\"style=\"display:none\"/g" $score_report
+	fi
 }
 
 function show-vuln()
@@ -22,6 +32,7 @@ function show-vuln()
 	#replaces placeholder name with actual vuln name (obfuscation)
 	sed -i "s/$2/$3/g" $score_report
 	notify-send "Congrats!" "You Gained Points"
+	update-found
 }
 
 function hide-vuln()
@@ -32,15 +43,20 @@ function hide-vuln()
 	#replaces placeholder name (people should keep their own notes on the points they've gained)
 	sed -i "s/$2/$3/g" $score_report
 	notify-send "Uh Oh!" "You Lost Points"
+	update-found
 }
 
 function penalty()
 {
 	sed -i "s/id=\"$1\"style=\"display:none\"/id=\"$1\"style=\"display:block\"/g" $score_report
 	((total_found-=$4))
+	((total_pen+=1))
+		
         #replaces placeholder name (people should keep their own notes on the points they've gained)
         sed -i "s/$2/$3/g" $score_report
         notify-send "Uh Oh!" "You Lost Points"
+        update-found
+
 }
 
 function remove-penalty()
@@ -48,9 +64,13 @@ function remove-penalty()
 	#allows vuln name to be seen in score report
         sed -i "s/id=\"$1\"style=\"display:block\"/id=\"$1\"style=\"display:none\"/g" $score_report
         ((total_found+=$4))
+	((total_pen-1))
+	
         #replaces placeholder name with actual vuln name (obfuscation)
         sed -i "s/$2/$3/g" $score_report
         notify-send "Congrats!" "You Gained Points"
+        update-found
+
 }
 
 function notify-send()
@@ -78,13 +98,24 @@ function check()
 	fi
 }
 
+function check-pen()
+{
+	if ( eval $1 ); then
+		if ( cat $score_report | grep "id=\"$2\"" | grep "display:none" ); then
+			penalty "$2" "$2;" "$3" "$4"
+		fi
+	elif ( cat $score_report | grep "id=\"$2\"" | grep "display:block" ); then
+		remove-penalty "$2" "$3" "$2;" "$4"
+	fi
+}
+
 update-found
 
 while true
 do
-	update-found
+	#linux vulns
+	check 'cat /home/skipper/Desktop/Forensics1 | grep "i like to move it move it"' '1' 'Forensics 1 Correct +5' '5'
 	
-	check "IF STATEMET" "VULN NUM" "WHAT TO DISPLAY ON SCORE REPORT" "POINTS"
-
+	#wait 10 seconds
 	sleep 10
 done
