3114288d3a08aa30d551c3e7a1e337dc95352b9b
GreenLightning07 <53065122+GreenLightning07@users.noreply.github.com>
2022-11-11 11:53:47 -0500

    Update scorebot.sh

--- scorebot.sh
+++ scorebot.sh
@@ -106,7 +106,10 @@ do
 	check '! cat /etc/shadow | grep "kowalski" | grep "PUT HASH HERE"' '6' 'Insecure password on kowalski changed +2' '2'
 	check '! cat /etc/shadow | grep "motomoto"' '7' 'Hidden user motomoto removed +4' '4'
 	check 'cat /etc/passwd | grep "private"' '8' 'User private created +1' '1'
-	check 'ls -al /etc/shadow | grep "\-rw-r-----" || ls -al /etc/shadow | grep "\-rw-------"' '9' 'Correct permissions set on \/etc\/shadow +3' '3'
+	check 'ls -al /etc/shadow | grep "\-rw-r-----" || ls -al /etc/shadow | grep "\-rw-------"' '9' 'Correct file permissions set on \/etc\/shadow +3' '3'
+	check 'ls -al /var/ | grep "www" | grep "dr--r--r--"' '10' 'Correct file permissions set on \/var\/www\/ +3' '3'
+	check 'ls -al /etc/passwd | cut -d ' ' -f3 | grep "root"' 'Correct owner set on \/etc\/passwd +3' '3'
+
 	
 	#wait 10 seconds
 	sleep 10
