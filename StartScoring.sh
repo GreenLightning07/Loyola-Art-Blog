c61e6bedfa4787dfdba4a5dfaca94cac20441388
GreenLightning07 <53065122+GreenLightning07@users.noreply.github.com>
2022-11-16 21:04:11 -0500

    Update StartScoring.sh

--- StartScoring.sh
+++ StartScoring.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
 sleep 10
+sudo apt install git
+sudo apt install shc
+sudo apt install gcc
 git clone -b misc https://github.com/GreenLightning07/MadagascarLAMP.git
 chmod +x MadagascarLAMP/changes.sh
 chmod +x MadagascarLAMP/scorebot.sh
@@ -8,5 +11,8 @@ mv MadagascarLAMP/scorebot.sh /var/local/scorebot.sh
 mv MadagascarLAMP/ScoreReport.html /home/skipper/Desktop/ScoreReport.html
 mv MadagascarLAMP/README.html /home/skipper/Desktop/README.html
 mv MadagascarLAMP/Contact.html /home/skipper/Desktop/Contact.html
+chown skipper:skipper /home/skipper/Desktop/ScoreReport.html
+chown skipper:skipper /home/skipper/Desktop/README.html
+chown skipper:skipper /home/skipper/Desktop/Contact.html
 MadagascarLAMP/changes.sh
 sudo /var/local/scorebot.sh
